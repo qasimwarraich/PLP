50581b0ffc811905b334f8e1009207b0d106b14f
qasimwarraich <qasim.warraich@gmail.com>
2021-04-05 13:41:16 +0200

    First steps into template quicksort

---
+++ A2/Task1/quicksort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+/* void swap(int* a, int* b); */ 
+
+template <class T>
+void print_array(std::vector <T> &arr);
+
+template <class T>
+int partition(std::vector <T> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+
+    for (int j = i + 1; j <= high; j++) {
+        if (arr[j] <= pivot){
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i], arr[low]);
+    return i; 
+}
+
+template <class T>
+void quicksort(std::vector <T> &arr, int low, int high)
+{
+     if (low < high){
+
+        int pivot = partition(arr, low, high);
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot + 1, high);
+    }
+}
+
+
+/* void swap(int* a, int* b) */
+/* { */
+/*     int t = *a; */
+/*     *a = *b; */
+/*     *b = t; */
+/* } */
+
+template <class T>
+void print_array(std::vector <T> &arr)
+{
+    for (int i = 0; i <= arr.size() - 1; i++) 
+        std::cout << arr[i] << '\n';
+}
+
+int main()
+{
+    std::vector <int> v_int = {10, 2, 3, 4, 1, 18, 420};
+
+    quicksort(v_int, 0, v_int.size() - 1);
+    std::cout << "spam = " << v_int.size() << '\n';
+    print_array(v_int); 
+    return 0;
+}
