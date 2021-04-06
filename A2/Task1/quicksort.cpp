537325f62af80a3a6fa96b77710f4eba41d3a617
qasimwarraich <qasim.warraich@gmail.com>
2021-04-06 14:50:12 +0200

    Template Quicksort working for ints, doubles, chars and strings.

--- A2/Task1/quicksort.cpp
+++ A2/Task1/quicksort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <string>
 
 template <class T>
 void print_array(std::vector <T> &arr);
@@ -8,7 +8,7 @@ void print_array(std::vector <T> &arr);
 template <class T>
 int partition(std::vector <T> &arr, int low, int high)
 {
-    int pivot = arr[low];
+    auto pivot = arr[low];
     int i = low;
 
     for (int j = i + 1; j <= high; j++) {
@@ -33,19 +33,37 @@ void quicksort(std::vector <T> &arr, int low, int high)
 }
 
 
-template <class T>
-void print_array(std::vector <T> &arr)
-{
-    for (int i = 0; i <= arr.size() - 1; i++) 
-        std::cout << arr[i] << '\n';
-}
 
 int main()
 {
-    std::vector <int> v_int = {10, 2, 3, 4, 1, 18, 420};
-
+    std::vector <int> v_int = {10, 2, 3, 4, 1, 18, 420, 300, 4000, 3, 1, 8};
     quicksort(v_int, 0, v_int.size() - 1);
-    std::cout << "spam = " << v_int.size() << '\n';
     print_array(v_int); 
+
+
+    std::vector <double> v_dbl = {10.1, 2.2, 3.3, 4.2, 1.3, 1.8, 44.2, 300.1, 400.0, 3.34, 11.0, 8.1};
+    quicksort(v_dbl, 0, v_dbl.size() - 1);
+    print_array(v_dbl);
+
+    std::vector <char> v_char = {'x', 'a', 'd', 'p'};
+    quicksort(v_char, 0, v_char.size() - 1);
+    print_array(v_char);
+
+    std::vector <std::string> v_str = {"Bjarne", "What", "ahy", "Have", "You", "Done", "Damn", "Don't"};
+    quicksort(v_str, 0, v_str.size() - 1);
+    print_array(v_str);
     return 0;
 }
+
+
+
+template <class T>
+void print_array(std::vector <T> &arr)
+{
+    std::cout << "Vector of type --> " << typeid(arr[0]).name() << '\n';
+    for (int i = 0; i <= (int)arr.size() - 1; i++) 
+       std::cout << arr[i] << ' ';
+    std::cout << '\n';
+}
+
+
