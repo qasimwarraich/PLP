a04d5a87ba9643eabae3397c3a0018478ac26c2a
qasimwarraich <qasim.warraich@gmail.com>
2021-04-13 21:49:41 +0200

    Added docs for statemachine and made some further progress with parsing and some state logic.

---
+++ A2/Task3/docs/README.md
@@ -0,0 +1,23 @@
+
+# A2:T3: Finite State Machine
+
+## Programming Languages and Paradigms Seminar
+
+### Approach
+
+I found this assignment to be quite difficult. Not really anything related to the actual difficulty of implementing an FSM but more so in how to deal with this unique assignment using `c++`. I spent an embarrassing amount of time doing string gymnastics and researching and researching on how to properly process the input files in a way that I could then map key value pairs for my implementation. I am not even sure if this is the optimal approach but I was a little overwhelmed by the parser side of this assignment that I wasn't really able to properly implement the FSM. That being said it was definitely a very good learning exercise for me and the frustrations lead me down a lot of different paths that definitely forced me to learn a lot more about `c++`. I could easily spend another week on this, researching and trying out the right approach. I hope you consider the effort I invested into this and just want to apologise for not being able to solve the full exercise. In particular I could not really find an intelligent way to map the transitions into a data structure that I could then use. Perhaps I should have created a generic class or some sort of struct to deal with this but making everything play well together with the file stream is quite a challenge. The program I have written compiles and runs without any warnings or notes but it only implements two most trivial states, `ready` and `exit`. 
+
+### Running the program
+
+Navigate to the directory and run the `make` command. This will produce a
+binary executable for you to run like `./a.out`.
+
+### Deleting the program
+
+You may use the `make clean` command to delete the produced binary.
+
+### Screenshot
+
+![lottery](../img/lottery1.png)
+
+![lottery Validation](../img/lottery2.png)
--- A2/Task3/statemachine.cpp
+++ A2/Task3/statemachine.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -8,43 +9,14 @@
 
 void print_map(std::map<std::string, std::string>& m);
 
-std::map<std::string, std::string> parse_transitions()
-{
-    std::ifstream infile("vending.machine");
-    std::string title1 = "[States]";
-    std::string title2 = "[Transitions]";
-    std::string temp;
-    std::map<std::string, std::string> transitions;
 
-
-    while ( std::getline(infile, temp) ){
-
-        if (temp.length() == 0)
-            continue;
-        if (temp[0] == '#')
-            continue;
-        if (temp.compare(title1) == 0)
-            continue;
-        if (temp.compare(title2) == 0)
-            break;
-        if (temp.find('#') < temp.length())
-            temp.erase(temp.find('#'));
-
-
-        auto colonpos = temp.find(':');
-        auto key = temp.substr(temp.find_first_not_of(" "), colonpos);
-        auto val = temp.substr(colonpos + 1, temp.length());
-
-        transitions[key.substr(0, key.find_first_of(" "))] =  val.substr(val.find_first_not_of(" "),val.length());
-    }
-    return transitions;
-}
 
 std::map<std::string, std::string> parse_states()
 {
     std::ifstream infile("vending.machine");
     std::string title1 = "[States]";
     std::string title2 = "[Transitions]";
+    std::string chars = "+*";
     std::string temp;
     std::map<std::string, std::string> states;
 
@@ -67,38 +39,90 @@ std::map<std::string, std::string> parse_states()
         auto key = temp.substr(temp.find_first_not_of(" "), colonpos);
         auto val = temp.substr(colonpos + 1, temp.length());
 
+        for (char c : chars)
+            key.erase(std::remove(key.begin(), key.end(), c), key.end());
+
         states[key.substr(0, key.find_first_of(" "))] =  val.substr(val.find_first_not_of(" "),val.length());
     }
 
-    /* print_map(states); */
     return states;
+}
+
+std::map<std::string, std::string> parse_transitions()
+{
+    /* Not properly implemented yet*/
+    std::ifstream infile("vending.machine");
+    std::string title1 = "[States]";
+    std::string title2 = "[Transitions]";
+    std::string temp;
+    std::map<std::string, std::string> transitions;
+
+
+    while ( std::getline(infile, temp) ){
+
+        if (temp.length() == 0)
+            continue;
+        if (temp[0] == '#')
+            continue;
+        if (temp.compare(title1) == 0)
+            continue;
+        if (temp.compare(title2) == 0)
+            continue;
+        if (temp.find('#') < temp.length())
+            temp.erase(temp.find('#'));
+
+
+        auto colonpos = temp.find(':');
+        auto key = temp.substr(temp.find_first_not_of(" "), colonpos);
+        auto val = temp.substr(colonpos + 1, temp.length());
+
+        transitions[key.substr(0, key.find_first_of(" "))] =  val.substr(val.find_first_not_of(" "),val.length());
+    }
+    return transitions;
 }
 
 int main()
 {
     std::map<std::string, std::string> states = parse_states();
     std::map<std::string, std::string> transitions = parse_transitions();
-    print_map(transitions);
+    print_map(states);
 
-/*     bool exit = 0; */
+    bool exit = 0;
     
-/*     while (!exit) { */
+        auto current_state = states["Ready"];
+        auto previous_state = current_state;
 
-/*         std::string input; */
-/*         auto current_state = states["*Ready"]; */
+    while (!exit) {
 
-/*         if (current_state == states["*Ready"]){ */
-/*             std::cout << "[Ready] " << current_state << '\n'; */
-/*             std::cin >> input; */
-/*             current_state = states[input]; */
-/*         } */
-/*         if (current_state == states["+Exit"]) { */
-/*             std::cout << "[Exit] " << current_state << '\n'; */
-/*             exit = 1; */
-/*             break; */
-/*         } */
+        std::string input;
 
-/*     } */
+        if (current_state == states["Ready"]){
+            std::cout << "[Ready] " << current_state << '\n';
+            std::cin >> input;
+            current_state = states[input];
+        }
+        if (current_state == states["Exit"]) {
+            std::cout << "[Exit] " << current_state << '\n';
+            exit = 1;
+            break;
+        }
+        if (current_state == states["Select"]) {
+            std::cout << "[Select] " << current_state << '\n';
+            std::cin >> input;
+            previous_state = current_state;
+            current_state = states[input];
+        }
+        if (current_state == states["Dispense"]) {
+            std::cout << "[Dispense] " << current_state << '\n';
+            std::cin >> input;
+            previous_state = current_state;
+            current_state = states[input];
+        }
+        else{
+            std::cout << "Invalid Input, please try again" << '\n';
+            current_state = previous_state;
+        }
+    }
 
     return 0; 
 }
@@ -107,7 +131,7 @@ int main()
 void print_map(std::map<std::string, std::string>& m)
 {
     for (const auto& x : m) {
-        std::cout << x.first << "--->" << x.second << "\n";
+        std::cout << "Key: " << x.first << "\tVal: " << x.second << "\n";
     }
     std::cout << "\n";
 }
