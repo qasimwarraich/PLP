d7f93671e1094043ac7e199eb3708074184b8896
qasimwarraich <qasim.warraich@gmail.com>
2021-04-26 19:33:17 +0200

    Map storing graphics as long strings working.

--- A3/textgame.cpp
+++ A3/textgame.cpp
@@ -1,9 +1,13 @@
 #include <cstdio>
-#include <vector>
+#include <ostream>
+#include <map>
 #include <fstream>
 #include <string>
-#include <list>
 #include <iostream>
+
+
+
+void print_map(std::map<std::string, std::string>& m);
 
 
 void parser()
@@ -33,31 +37,45 @@ void parser()
  
     }    
     std::string temp2;
-    std::vector<std::list<std::string>> spam;
-    std::list<std::string> gfx;
+    std::string graphic_of_state;
+    std::map<std::string, std::string> graphics_map;
+
         while (std::getline(infile2, temp2)){
             if (temp2[0] == '@') {
+                std::string title = temp2.substr(temp2.find_first_not_of("@*+"));
+                /* std::cout << "title is " << title << '\n'; */
 
                 while (std::getline(infile2, temp2)){
                     if (temp2[0] == '>'){
-                        std::cout << "spam" << '\n';
+                        /* std::cout << "spam" << '\n'; */
                         break;
                     }
-                    gfx.push_back(temp2);
+                    graphic_of_state.append(temp2 + '\n');
+                    graphics_map[title] = graphic_of_state;
                 }
-                    spam.push_back(gfx);
+                
+                graphic_of_state.clear();
             }
         }
 
-        for (auto& i : gfx)
-            std::cout << "image" << i << '\n';
-        /* for (auto& j : spam) */
-        /*     std::cout << "vector" << j.first() << '\n'; */
+        print_map(graphics_map); 
 }
+
+
+
+void print_map(std::map<std::string, std::string> &m)
+{
+    for (const auto& x : m) {
+        std::cout << "Key: " << x.first << "\tVal: " << x.second << "\n";
+    }
+    std::cout << "\n";
+}
+
 
 int main()
 {
     parser();
+
 
     return 0;
 }
