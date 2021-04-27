7548045190f6cbce888e4780c1d7252bdd76d8c7
qasimwarraich <qasim.warraich@gmail.com>
2021-04-27 10:50:52 +0200

    Added game loop logic, state machine is mostly working now

--- A3/textgame.cpp
+++ A3/textgame.cpp
@@ -5,21 +5,68 @@
 #include <string>
 #include <iostream>
 
-
-void print_map(std::multimap<std::string, std::string>& m);
-
-
-void parser()
+std::map<std::string, std::string> parse_graphics()
 {
+    /* This part aims to isolate the graphic and store it as a long string with 
+     * appened newline chars to keep formatting
+     */
+    std::ifstream infile("doggo.gfl");
+    std::string temp;
+    std::string graphic_of_state;
+    std::map<std::string, std::string> graphics_map;
 
+    while (std::getline(infile, temp)){
+        if (temp[0] == '@') {
+            /* Get title of graphic as key for the map */
+            std::string title = temp.substr(temp.find_first_not_of("@*+"));
+            title = title.erase(title.find(':'));
+
+            /* Stop when a state transition line detected */
+            while (std::getline(infile, temp)){
+                if (temp[0] == '>')
+                    break;
+                
+                graphic_of_state.append(temp + '\n');
+                graphics_map[title] = graphic_of_state;
+            }
+            graphic_of_state.clear();
+        }
+    }
+    return graphics_map;
+}
+
+void print_map(std::multimap<std::string, std::string> &m)
+{
+    for (const auto& x : m) {
+        std::cout << "Key: " << x.first << "\t\tVal: " << x.second << "\n";
+    }
+    std::cout << "\n";
+}
+
+void print_graphics_map(std::map<std::string, std::string> &m)
+{
+    for (const auto& x : m) {
+        std::cout << "Key: " << x.first << "\t\tVal: " << x.second << "\n";
+    }
+    std::cout << "\n";
+}
+
+int main(int argc, char *argv[])
+{
+    /* std::cout << argv[0] << '\n'; */
     std::ifstream infile("doggo.gfl");
     std::string temp;
     std::string state;
     std::string initial_state;
+    std::map<std::string, std::string> graphics_map;
     std::multimap<std::string, std::string> commands;
     std::multimap<std::string, std::string> messages;
     std::multimap<std::string, std::string> transitions;
 
+    graphics_map = parse_graphics();
+
+
+    /* Parse states, intial state, transitions and messages */
     while ( std::getline(infile, temp) ){
         if (temp.length() == 0)
             continue;
@@ -47,69 +94,35 @@ void parser()
             messages.insert(std::pair<std::string,std::string> (command, message));
             transitions.insert(std::pair<std::string,std::string> (command, transition));
         }
-    }    
+    }
 
-        /* print_map(commands); */ 
-        /* print_map(messages); */ 
-        /* print_map(transitions); */ 
-}
+    bool exit = 0;
+    std::string input;
 
-std::map<std::string, std::string> parse_graphics()
-{
-    /* This part aims to isolate the graphic and store it as a long string with 
-     * appened newline chars to keep formatting
-     */
-    std::ifstream infile2("doggo.gfl");
-    std::string temp2;
-    std::string graphic_of_state;
-    std::map<std::string, std::string> graphics_map;
+    std::cout << "**Welcome to a text based adventure game**" << "\n\n\n";
+    std::string current_state = initial_state;
+    
+    while (!exit) {
 
-    while (std::getline(infile2, temp2)){
-        if (temp2[0] == '@') {
-            /* Get title of graphic as key for the map */
-            std::string title = temp2.substr(temp2.find_first_not_of("@*+"));
-            title = title.erase(title.find(':'));
 
-            /* Stop when a state transition line detected */
-            while (std::getline(infile2, temp2)){
-                if (temp2[0] == '>')
-                    break;
-                
-                graphic_of_state.append(temp2 + '\n');
-                graphics_map[title] = graphic_of_state;
+        std::cout << graphics_map[current_state];
+        if (current_state == "GameOver")
+            break;
+        std::cout << "> Doggo is currently [" << current_state << "] Enter a command from above\n";
+        /* std::cin >> input; */ 
+        std::getline(std::cin, input);
+        if (input == "quit" ) {
+            exit = 1;
+            break;
+        }
+        else {
+            if (messages.find(input) != messages.end()){ 
+                std::cout << "\n\n=>" << messages.find(input)->second << " <=\n";
+                current_state = transitions.find(input)->second;
             }
-            graphic_of_state.clear();
+            else
+                std::cout << "**INVALID INPUT, TRY AGAIN**\n";
         }
     }
-    return graphics_map;
-}
-
-
-void print_map(std::multimap<std::string, std::string> &m)
-{
-    for (const auto& x : m) {
-        std::cout << "Key: " << x.first << "\t\tVal: " << x.second << "\n";
-    }
-    std::cout << "\n";
-}
-
-void print_graphics_map(std::map<std::string, std::string> &m)
-{
-    for (const auto& x : m) {
-        std::cout << "Key: " << x.first << "\t\tVal: " << x.second << "\n";
-    }
-    std::cout << "\n";
-}
-
-int main(int argc, char *argv[])
-{
-    std::map<std::string, std::string> graphics_map;
-    /* std::cout << argv[0] << '\n'; */
-    parser();
-    
-    graphics_map = parse_graphics();
-    /* print_graphics_map(graphics_map); */
-    std::cout << graphics_map["Sleeping"]; 
-
     return 0;
 }
