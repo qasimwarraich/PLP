54d857e2ddd20566f844e1ee2cc90c1849ac871a
qasimwarraich <qasim.warraich@gmail.com>
2021-04-29 19:32:35 +0200

    Managed to stop segfaulting for hotel with some safeguard if statements
    - TODO: Fix parsing of graphics with no commands! (RestroomCorridor)

--- A3/textgame.cpp
+++ A3/textgame.cpp
@@ -5,12 +5,12 @@
 #include <string>
 #include <iostream>
 
-std::map<std::string, std::string> parse_graphics()
+std::map<std::string, std::string> parse_graphics(std::string gamefile)
 {
     /* This part aims to isolate the graphic and store it as a long string with 
      * appened newline chars to keep formatting
      */
-    std::ifstream infile("doggo.gfl");
+    std::ifstream infile(gamefile);
     std::string temp;
     std::string graphic_of_state;
     std::map<std::string, std::string> graphics_map;
@@ -19,11 +19,14 @@ std::map<std::string, std::string> parse_graphics()
         if (temp[0] == '@') {
             /* Get title of graphic as key for the map */
             std::string title = temp.substr(temp.find_first_not_of("@*+"));
-            title = title.erase(title.find(':'));
+            if (title.find(':') < title.length())
+                title = title.erase(title.find(':'));
 
             /* Stop when a state transition line detected */
             while (std::getline(infile, temp)){
                 if (temp[0] == '>')
+                    break;
+                if (temp[0] == '@')
                     break;
                 
                 graphic_of_state.append(temp + '\n');
@@ -33,6 +36,7 @@ std::map<std::string, std::string> parse_graphics()
         }
     }
     return graphics_map;
+
 }
 
 void print_map(std::multimap<std::string, std::string> &m)
@@ -46,7 +50,7 @@ void print_map(std::multimap<std::string, std::string> &m)
 void print_graphics_map(std::map<std::string, std::string> &m)
 {
     for (const auto& x : m) {
-        std::cout << "Key: " << x.first << "\t\tVal: " << x.second << "\n";
+        std::cout << "Key: " << "'" << x.first << "'" << "\t\tVal: " << x.second << "\n";
     }
     std::cout << "\n";
 }
@@ -70,8 +74,9 @@ int main(int argc, char *argv[])
     std::multimap<std::string, std::string> messages;
     std::multimap<std::string, std::string> transitions;
 
-    graphics_map = parse_graphics();
+    graphics_map = parse_graphics(gamefile);
 
+    print_graphics_map(graphics_map);
 
     /* Parse states, intial state, transitions and messages */
     while ( std::getline(infile, temp) ){
@@ -82,9 +87,16 @@ int main(int argc, char *argv[])
             if ( temp.find('*') < temp.length()  ){
                 initial_state = temp.substr(temp.find_first_not_of("@*+"));
                 initial_state = initial_state.replace(initial_state.find_first_of(":"),1,"");
+                
+            if ( initial_state.find(' ') < initial_state.length())
+                initial_state = initial_state.erase(initial_state.find(' '));
                 }
             state = temp.substr(temp.find_first_not_of("@*+"));
-            state = state.replace(state.find_first_of(":"),1,"");
+            if (state.find_first_of(':') < state.length())
+                state = state.replace(state.find_first_of(':'),1,"");
+
+            if ( state.find(' ') < state.length())
+                state = state.erase(state.find(' '));
             /* std::cout << state << '\n'; */
         }
         /* Parsing Possible Transitions */
